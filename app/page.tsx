c6c2845fef739a126060136c58f023a72f3fc994
Malicious-Bunny <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2024-06-08 19:26:32 +0100

    Installed SEO tools configurieng now

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
+'use client'
+
 import Hero from "@/components/hero";
 import Blogsection from "@/components/blogsection";
 import client from "@/lib/client";
+import { useEffect } from "react";
 
-export default async function Page() {
+async function getData(){
+
+    const MainUrl = 'https://cdn.contentful.com/spaces/yqf5x6omucvn/environments/master/entries/AnK2qvSnHRIROQdUTDE4m?access_token=u5YLhoRiw6rnI_Qt0FMsHLfIcTnpxBQAD3yPSN9M72s'
+
+    const res = await fetch(MainUrl)
+    const data = await res.json()
+
+    return data
+
+}
+export default function Page() {
     //fetching posts with the contentful client
-    client.getEntry('AnK2qvSnHRIROQdUTDE4m')
-  .then((entry) => console.log(entry))
-  .catch(console.error)
-
- 
-
     
+   useEffect(() => {
+    getData()
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+   }, [])
+   
     return (
         <div className="w-screen flex flex-col content-center">
             
