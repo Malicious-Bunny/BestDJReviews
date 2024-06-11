2aafba4bd409a0fb014504e1137782d176f6d31d
Malicious-Bunny <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2024-06-11 13:42:40 +0100

    All done taking care of SEO now

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -14,7 +14,7 @@ export default async function Page(){
         <div className="w-screen flex flex-col content-center">
            
             <main className="xl:w-[76%] p-4 w-full xl:my-12 flex-col flex gap-16  self-center">
-            <h1 className="scroll-m-20  xl:text-5xl self-start text-3xl font-bold tracking-tight lg:text-5xl">
+            <h1 className="scroll-m-20  xl:text-3xl self-start text-3xl font-bold tracking-tight lg:text-5xl">
                         Latest whats happening in the DJ world
                 </h1>
             <section
--- app/posts/[postid]/page.tsx
+++ app/posts/[postid]/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image"
- 
+import Image from "next/image";
+import client from "@/lib/client";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,80 +7,105 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Badge } from "@/components/ui/badge"
-import { FaCircle } from "react-icons/fa"
-export default function Page() {
-    return ( 
-        <div className="w-screen flex flex-col content-center">
-            
-            <main className="xl:w-[78%] p-4 w-full  justify-center my-4 flex-col flex xl:gap-20  self-center">
-           
-           
-            <div className="head flex flex-col self-center content-center justify-center gap-8">
-                <Linkcrumb  links={["home","posts","page"]} />
+} from "@/components/ui/breadcrumb";
+import { Badge } from "@/components/ui/badge";
+import { FaCircle } from "react-icons/fa";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { format } from "date-fns";
+export default async function Page({ params }: any) {
+  //get the post id from the params and fetch the data for that particular post
+  const { postid } = params;
+  const Data = await client.getEntry(postid);
 
-                <h1 className="scroll-m-20 self-center xl:text-6xl text-3xl font-bold tracking-tight lg:text-5xl">
-                        DJ tools becoming extremely pricy and heres why
-                </h1>
-                  <Image className=" self-center" src="/djhero.jpg" width={1000} height={800} alt={""} />
-           <div className=" flex flex-row justify-between content-center">
-            <div className="badges gap-2 flex flex-row ">
+  const imgUrl = "https:" + (Data.fields.image?.fields?.file?.url || "");
 
-                <Badge style={
-                    {
-                        color : "#FF7A00",
-                        borderColor : "#FF7A00"
-                        
-                    }
-                } variant={'outline'} className="hover:text-black border-2 cursor-pointer">Music</Badge>
-                
-                <Badge style={
-                    {
-                        color : "#FF7A00",
-                        borderColor : "#FF7A00"
-                        
-                    }
-                } variant={'outline'} className="hover:text-black border-2 cursor-pointer">DJ</Badge>
-            </div>
-            <div className="flex flex-row font-extrabold gap-4 self-start justify-center content-center" >
-                <div className="date">
-                    <p className="xl:text-lg text-md text-gray-400">April 24,2024</p>
-                </div>
-                <div className="bullet self-center">
-                    <FaCircle  style={
+  console.log(Data.fields)
+
+  return (
+    <div className="w-screen flex flex-col content-center">
+      <main className="xl:w-[78%]  p-4 w-full  justify-center my-4 flex-col gap-8 flex xl:gap-12  self-center">
+        <div className="head flex flex-col self-center w-full content-center justify-center gap-8">
+          
+
+          <h1 className="scroll-m-20 self-center xl:text-4xl text-3xl font-bold tracking-tight lg:text-5xl">
+            {String(Data.fields.title)}
+          </h1>
+          <Image
+            className=" self-center"
+            src={imgUrl}
+            width={1000}
+            height={1000}
+            alt={""}
+          />
+        <div className=" flex flex-row justify-between content-center">
+        <div className="flex flex-row gap-2">
+                { Data.fields.category.map((tag:any) => (
+                    <Badge key={tag} style={
                         {
                             color : "#FF7A00",
-                        }
-                    } />
-                </div>
-                <div className="category">
-                    <p className="xl:text-lg text-md text-gray-400">News</p>
-                </div>
+                            backgroundColor: "white",
+                            borderColor : "#FF7A00"
+                            
+                        } }>{tag}</Badge>
+                ))}
+                
             </div>
-           </div>
+                
+                
+
+
+
+            <div className="flex flex-row font-extrabold gap-4 self-start justify-center content-center">
+              <div className="date">
+                <p className="xl:text-lg text-md text-gray-400">
+                  {format(String(Data.fields.date), "MMMM dd, yyyy")}
+                </p>
+              </div>
+              <div className="bullet self-center">
+                <FaCircle
+                  style={{
+                    color: "#FF7A00",
+                  }}
+                />
+              </div>
+              <div className="category">
+                <p className="xl:text-lg text-md text-gray-400">
+                  {String(Data.fields.mainCategory)}
+                </p>
+              </div>
+            </div>
            
-            </div>
-            </main>
+          </div>
         </div>
-    )
+
+
+
+        <div className="content">
+          {documentToReactComponents(Data.fields.post)}
+        </div>
+      </main>
+    </div>
+  );
 }
 export interface breadcrumProps {
-    links : string[]
+  links: string[];
 }
 
-export function Linkcrumb(links:breadcrumProps){
-    return (
-        <>
-         <Breadcrumb className="self-center text-xl">
-      <BreadcrumbList className=" text-[#FF7A00] text-lg xl:text-xl">
-        {links.links.map((link) => (
-          <BreadcrumbItem key={link}>
-            <BreadcrumbLink href={`/${link}`}>{link}</BreadcrumbLink>
-        {links.links.indexOf(link) === links.links.length - 1 ? null : <BreadcrumbSeparator/>}
+export function Linkcrumb(links: breadcrumProps) {
+  return (
+    <>
+      <Breadcrumb className="self-center text-xl">
+        <BreadcrumbList className=" text-[#FF7A00] text-lg xl:text-xl">
+          {links.links.map((link) => (
+            <BreadcrumbItem key={link}>
+              <BreadcrumbLink href={`/${link}`}>{link}</BreadcrumbLink>
+              {links.links.indexOf(link) === links.links.length - 1 ? null : (
+                <BreadcrumbSeparator />
+              )}
             </BreadcrumbItem>
-        ))}
-      </BreadcrumbList>
-    </Breadcrumb></>
-    )
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </>
+  );
 }
